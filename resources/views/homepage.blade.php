750ff0ec1a0539244d86af2d0774a7043af27ab4
NeirAzer <rezkyazharsuryaputra@gmail.com>
2026-02-17 10:58:45 +0700

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="text-3xl tracking-tight text-gray-900">Welcome to the <span class="text-blue-800
+        font-bold">{{$title}}</span></h1>
+@endsection
