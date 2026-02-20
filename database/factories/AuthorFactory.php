7bc8580c938905427c6e9bb6c931e052b4af7ad1
NeirAzer <rezkyazharsuryaputra@gmail.com>
2026-02-20 10:36:29 +0700

    factory

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
+ */
+class AuthorFactory extends Factory
+{
+    
+    public function definition(): array
+    {
+        $name = fake()->name();
+        return [
+            'name' => $name,
+            'slug' => str()->slug($name),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
         ]);
+
+        \App\Models\Author::factory(10)->create();
+        \App\Models\Book::factory(7)->create();
     }
 }
