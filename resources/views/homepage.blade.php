274f7c39c0a15809472154b6d74e403bb769991a
NeirAzer <rezkyazharsuryaputra@gmail.com>
2026-04-17 10:38:24 +0700

    API store, update, & destroy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,8 +6,8 @@
     <section class="pt-10 bg-gray-100 sm:pt-16 lg:pt-24">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div class="max-w-2xl mx-auto text-center">
-                <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">Real humans are here to help you building your brand</h2>
-                <p class="mt-6 text-lg text-gray-900">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+                <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">Welcome to <span class="text-blue-700 font-bold">E-Library</span></h2>
+                <p class="mt-6 text-lg text-gray-900">Open minded for All people.</p>
                 <a href="/hall" title="" class="inline-flex items-center justify-center px-6 py-4 mt-12 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:bg-blue-700" role="button">
                   <i class="fa-solid fa-book-open mr-2"></i> 
                       Read now
